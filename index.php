f7d49ca5b850bc7ada598a79c7ae008f9ae5778f
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-12 12:04:34 +0100

    Création d'une liste avec une boucle for.

--- index.php
+++ index.php
@@ -5,4 +5,10 @@
  * Date: 12/02/2019
  * Time: 09:57
  */
-phpinfo();
+$message = "<h1>Je fais du PHP - boucle <em>for</em></h1> ";
+$message .= "<ul>";
+for($i = 0; $i < 11; $i++){
+$message .= "<li>" . $i . "</li>";
+}
+$message .= "</ul>";
+echo $message;
