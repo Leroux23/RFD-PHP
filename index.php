7f8b31f9fae428353b7d8cf4fdf3412e19cb3b08
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-12 12:30:00 +0100

    Création variable superglobale avec une récupération des paramètres URL > $_GET.

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
+<ul>
+    <li><a href="index.php?page=truc">Truc</a></li>
+    <li><a href="index.php?page=machin">Machin</a></li>
+    <li><a href="index.php?page=michel">Michel</a></li>
+</ul>
 <?php
-/**
- * Created by PhpStorm.
- * User: Salomé
- * Date: 12/02/2019
- * Time: 09:57
- */
-$message = "<h1>Je fais du PHP - boucle <em>for</em></h1> ";
-$message .= "<ul>";
-for($i = 0; $i < 11; $i++){
-$message .= "<li>" . $i . "</li>";
+if (isset($_GET['page'])){
+    $page = $_GET['page'];
+    echo $page;
 }
-$message .= "</ul>";
-echo $message;
+else{
+    echo "Message";
+}
