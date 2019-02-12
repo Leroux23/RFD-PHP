41daf810bbc346d91774d0bf99f0c0ef92546887
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-12 15:33:12 +0100

    Création de fichiers menus, pour développer les liens directs.

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <header>
     <nav>
         <ul>
-            <li><a href=index.php?pages=accueil">Menu 1</a></li>
-            <li><a href=index.php?pages=produits">Menu 2</a></li>
-            <li><a href=index.php?pages=inscription">Menu 3</a></li>
+            <li><a href="index.php?pages=accueil">Menu 1</a></li>
+            <li><a href="index.php?pages=produits">Menu 2</a></li>
+            <li><a href="index.php?pages=inscription">Menu 3</a></li>
             <li><a href="index.php?pages=contact">Menu 4</a></li>
         </ul>
     </nav>
