c6cc77d7ae57f38bda3db802b81cb3438bb18b93
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-13 16:46:36 +0100

    Correction formulaire. Intégration style.css pour le formulaire.

--- includes/frminscription.php
+++ includes/frminscription.php
@@ -1,16 +1,19 @@
-<form method="post" action="index.php?page=inscription">
+<form method="post" action="index.php?pages=inscription">
     <div>
-        <label for="nom">Nom&nbsp; :</label>
+        <label for="nom">Nom&nbsp;: </label>
         <input type="text" id="nom" name="nom" />
     </div>
     <div>
-        <label for="mail">Adresse mail&nbsp; :</label>
+        <label for="mail">Mail&nbsp;: </label>
         <input type="text" id="mail" name="mail" />
     </div>
     <div>
-        <label for="mdp">Mot de passe&nbsp; :</label>
+        <label for="mdp">Mot de passe&nbsp;: </label>
         <input type="password" id="mdp" name="mdp" />
     </div>
-    <input type="reset" value="Effacer" />
-    <input type="submit" value="Envoyer" onclick="validerFrm()"/>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+    <input type="hidden" name="maurice" />
 </form>
